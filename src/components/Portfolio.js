27f68b758268ae2690c5404591f15b7a801a12a0
eparkhurst <eli.parkhurst@gmail.com>
2017-02-08 10:12:36 -0700

    removed carousel component and moved everything into portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Carousel from './Carousel'
 import brewsBrothers from '../imgs/brewsbrothers.png'
 import moviePicker from '../imgs/moviepicker.png'
 import trackPath from '../imgs/trackpath.png'
