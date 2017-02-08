dc481a486575d846516edee435dca5a9ea3cecb8
eparkhurst <eli.parkhurst@gmail.com>
2017-02-08 15:47:45 -0700

    changed css so that 'contact me' is centered on smaller screens and removed white space from bottom of cotact me section

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ class About extends Component{
           I’m a Colorado kid doing my best to grow along with Denver. Professionally, I am a full stack developer with an emphasis on the front end. I dream in React but AngularJS and I still have a good relationship. Don’t put me in a box though. I’ve made RESTful APIs, native mobile apps and know my way around an ERD. If I look like a good fit for your team my contact info is on this very page!
         </p>
       </div>
-      <div className="about-panel">
+      <div className="about-panel contact-me">
         <h1>Contact Me</h1>
         <a href="https://www.linkedin.com/in/eliparkhurst" target="_blank" className="connect-link"><span className="fa fa-linkedin-square"></span> Linked In</a>
         <a href="https://github.com/eparkhurst" target="_blank" className="connect-link"><span className="fa fa-github-square"></span> Git Hub</a>
